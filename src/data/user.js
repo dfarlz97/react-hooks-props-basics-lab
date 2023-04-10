070efccfba578077a6cb2b0b946a78b1b4d87d69
+ <+>
2023-04-10 16:55:21 -0400

    satisfy deliverables

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
+  name: "Dennis",
   city: "New York",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/dfarlz97",
+    linkedin: "https://www.linkedin.com/in/dennis-farley-7a9244184/",
   },
 };
 
